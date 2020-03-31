34b7ee94fcb203e584b545d19406c7b40af4fb1e
Lucia Fabbri <lucia.fabbri13@studio.unibo.it>
2020-03-31 21:11:30 +0200

    Class MovementImpl and Interface Movement related to the Player added

---
+++ Movement.java
@@ -0,0 +1,22 @@
+package player.movement;
+
+import org.newdawn.slick.Input;
+
+import utility.Pair;
+
+/**
+ * 
+ * Interface that allows the player to move in the environment
+ * 
+ */
+
+public interface Movement {
+
+	/** 
+	 * 
+	 * this method needs the input from the container and the coordinates of the player 
+	 * It returns the new coordinates of the player 
+	 * 
+	 */
+	public Pair<Integer, Integer> movePlayer (Input input, Pair<Integer, Integer> pos);
+}
---
+++ MovementImpl.java
@@ -0,0 +1,72 @@
+package player.movement;
+
+import utility.Pair;
+
+import org.newdawn.slick.Input;
+
+/**
+ * Class that implements all the methods of Interface Movement
+ * 
+ * It allows the player to move in the environment: 
+ * based on the command pressed, it will be able to move in that direction
+ * 
+ */
+public class MovementImpl implements Movement {
+	
+	public MovementImpl() {
+	}
+		
+	@Override
+	public Pair<Integer, Integer> movePlayer(Input input, Pair<Integer,Integer> pos) {
+		
+		//control UP input   
+		if(input.isKeyDown(Input.KEY_W)) {
+			return new Pair<Integer,Integer>(pos.getX(),pos.getY() - 1);
+		}
+		
+		
+		//control DOWN input   
+		else if(input.isKeyDown(Input.KEY_S)) {
+			return new Pair<Integer,Integer>(pos.getX(),pos.getY() + 1);
+		}
+
+		
+		//control LEFT input   
+		else if(input.isKeyDown(Input.KEY_A)) {
+			return new Pair<Integer,Integer>(pos.getX() - 1,pos.getY());
+		}
+
+		
+		//control RIGHT input   
+		else if(input.isKeyDown(Input.KEY_D)) {
+			return new Pair<Integer,Integer>(pos.getX() + 1,pos.getY());
+		}
+		
+		
+		//control UP-RIGHT input
+		else if( input.isKeyDown(Input.KEY_W) && input.isKeyDown(Input.KEY_D)) {
+			return new Pair<Integer,Integer>(pos.getX() + 1,pos.getY() - 1);
+		}
+		
+		
+		//control UP-LEFT input
+		else if(input.isKeyDown(Input.KEY_W) && input.isKeyDown(Input.KEY_A)) {
+			return new Pair<Integer,Integer>(pos.getX() - 1,pos.getY() - 1);
+		}
+		
+		
+		//control DOWN-LEFT input
+		else if(input.isKeyDown(Input.KEY_S) && input.isKeyDown(Input.KEY_A)) {
+			return new Pair<Integer,Integer>(pos.getX() - 1,pos.getY() + 1);
+		}
+		
+		
+		//control DOWN-RIGHT input 
+		else if(input.isKeyDown(Input.KEY_S) && input.isKeyDown(Input.KEY_D)) {
+			return new Pair<Integer,Integer>(pos.getX() + 1,pos.getY() + 1);
+		}
+		
+		else throw new IllegalArgumentException();
+		
+	}
+}
