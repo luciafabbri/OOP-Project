011b7b629fb72f5278a6c525b071a23769745cf6
Lucia Fabbri <lucia.fabbri13@studio.unibo.it>
2020-05-01 11:04:35 +0200

    new comments added in entity.move

--- src/entity/move/CheckPosImpl.java
+++ src/entity/move/CheckPosImpl.java
@@ -5,6 +5,12 @@ import design.utilities.GameSettings;
 import design.utilities.Pair;
 import entity.Entity;
 
+/**
+ * Class that implements interface CheckPos used to check if an entity is going out of bounds or 
+ * is in collision with an obstacles in the current room
+ * According to his current direction, new dimensions will be set
+ */
+
 public class CheckPosImpl implements CheckPos, GameSettings{
 	
 	protected int leftPix;
@@ -13,6 +19,10 @@ public class CheckPosImpl implements CheckPos, GameSettings{
 	protected int upPix;
 	private Entity entity;
 	
+	/**
+	 * Default constructor
+	 * @param entity, the entity of which we have to check the next available position	 
+	 */
 	public CheckPosImpl(Entity entity) {
 		this.entity = entity;
 	}
@@ -23,7 +33,13 @@ public class CheckPosImpl implements CheckPos, GameSettings{
 		return !(isOutOfLimits(pos) || checkObstaclesRoom(room, pos));
 	}
 	
-	public boolean checkObstaclesRoom(RoomDesign room, Pair<Integer, Integer> pos) {
+	/**
+	 * Method used to check if an entity is in collision with an obstacle in the dungeon
+	 * @param room, entity's current room
+	 * @param pos, entity's current position
+	 * @return true if the entity had a collision with an obstacle
+	 */
+	protected boolean checkObstaclesRoom(RoomDesign room, Pair<Integer, Integer> pos) {
 		boolean checkX, checkY;
 		for (Pair<Integer, Integer> obst : room.getObstaclePositions()) {
 			checkX = pos.getX() + leftPix < obst.getX() + GameSettings.TILESIZE && pos.getX() + rightPix > obst.getX() ;
@@ -36,8 +52,9 @@ public class CheckPosImpl implements CheckPos, GameSettings{
 	}
 
 	/**
+	 * Method used to check if an entity is going out of dungeon's bounds
 	 * @param pos, entity's current coordinates 
-	 * @return 
+	 * @return true if the entity is out of bounds
 	 */
 	private boolean isOutOfLimits(Pair<Integer, Integer> pos) {
 		return ((pos.getX() + leftPix < LIMITLEFT || pos.getX() + rightPix > LIMITRIGHT)
@@ -45,7 +62,7 @@ public class CheckPosImpl implements CheckPos, GameSettings{
 	}
 
 	/**
-	 * Method used to update entity's dimensions in the dungeon
+	 * Method used to update entity's dimensions in the dungeon according to his current direction
 	 */
 	private void updateDimension() {
 		this.upPix = entity.getDimension().getUp();
--- src/entity/move/MovePos.java
+++ src/entity/move/MovePos.java
@@ -3,7 +3,7 @@ package entity.move;
 import design.utilities.Pair;
 
 /**
- * An interface 
+ * An interface used to set next entity's coordinates and direction in the dungeon 
  */
 
 public interface MovePos {
@@ -11,7 +11,7 @@ public interface MovePos {
 	/**
 	 * @param pos, entity's current coordinates 
 	 * @param dir, entity's current direction
-	 * @return the possible next entity's coordinates in the dungeon
+	 * @return the possible next entity's coordinates and direction
 	 */
 	Pair<Integer,Integer> nextPos(Pair<Integer,Integer> pos, Direction dir);
 
