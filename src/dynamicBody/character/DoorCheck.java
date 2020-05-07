d8929866c8e414ce7227f27eae937040b4ae508c
Federico Pirazzoli <federico.pirazzoli@studio.unibo.it>
2020-05-07 19:10:20 +0200

    Fixed boundaries issues with the doors

--- src/dynamicBody/character/DoorCheck.java
+++ src/dynamicBody/character/DoorCheck.java
@@ -20,7 +20,7 @@ public class DoorCheck {
 	 * @return true if the Player is inside the designated area, otherwise false
 	 */
 	public boolean transNorth(final Pair<Integer, Integer> pos) {
-		 return (pos.getX() > (GameSettings.TILESIZE * 9) - 11 && pos.getX() < (GameSettings.TILESIZE * 9 ) + 11) && (pos.getY() > -5 && pos.getY() < -2);
+		 return (pos.getX() > (GameSettings.TILESIZE * 9) - 11 && pos.getX() < (GameSettings.TILESIZE * 9 ) + 11) && (pos.getY() < -5);
 	}
 	
 	
@@ -30,7 +30,7 @@ public class DoorCheck {
 	 * @return true if the Player is inside the door boundaries, otherwise false
 	 */
 	public boolean doorWest(final Pair<Integer, Integer> pos) {
-		 return (pos.getX() > 0 && pos.getX() < GameSettings.TILESIZE) && (pos.getY() + 48 > (GameSettings.TILESIZE * 5) && pos.getY() + 48 < (GameSettings.TILESIZE * 5 + 44));
+		 return (pos.getX() < GameSettings.TILESIZE) && (pos.getY() + 48 > (GameSettings.TILESIZE * 5) && pos.getY() + 48 < (GameSettings.TILESIZE * 5 + 44));
 	}
 
 	/**
@@ -39,7 +39,7 @@ public class DoorCheck {
 	 * @returntrue if the Player is inside the designated area, otherwise false
 	 */
 	public boolean transWest(final Pair<Integer, Integer> pos) {
-		 return (pos.getX() > 0 && pos.getX() < GameSettings.TILESIZE / 4) && (pos.getY() + 48 > (GameSettings.TILESIZE * 5) && pos.getY() + 48 < (GameSettings.TILESIZE * 5 + 44));
+		 return (pos.getX() < GameSettings.TILESIZE / 4) && (pos.getY() + 48 > (GameSettings.TILESIZE * 5) && pos.getY() + 48 < (GameSettings.TILESIZE * 5 + 44));
 	}
 	
 	
@@ -49,7 +49,7 @@ public class DoorCheck {
 	 * @return true if the Player is inside the door boundaries, otherwise false
 	 */
 	public boolean doorEast(final Pair<Integer, Integer> pos) {
-		 return (pos.getX() > GameSettings.WIDTH - GameSettings.TILESIZE * 3 && pos.getX() < GameSettings.LIMITRIGHT) && (pos.getY() + 48 > (GameSettings.TILESIZE * 5) && pos.getY() + 48 < (GameSettings.TILESIZE * 5 + 44));
+		 return (pos.getX() > GameSettings.WIDTH - GameSettings.TILESIZE * 3  && (pos.getY() + 48 > (GameSettings.TILESIZE * 5) && pos.getY() + 48 < (GameSettings.TILESIZE * 5 + 44)));
 	}
 	
 	/**
@@ -58,7 +58,7 @@ public class DoorCheck {
 	 * @return true if the Player is inside the designated area, otherwise false
 	 */
 	public boolean transEast(final Pair<Integer, Integer> pos) {
-		 return (pos.getX() > GameSettings.WIDTH - (GameSettings.TILESIZE * 3) / 2 + 15 && pos.getX() < GameSettings.LIMITRIGHT) && (pos.getY() + 48 > (GameSettings.TILESIZE * 5) && pos.getY() + 48 < (GameSettings.TILESIZE * 5 + 44));
+		 return (pos.getX() > GameSettings.WIDTH - (GameSettings.TILESIZE * 3) / 2 + 15 && (pos.getY() + 48 > (GameSettings.TILESIZE * 5) && pos.getY() + 48 < (GameSettings.TILESIZE * 5 + 44)));
 	}
 	
 	
@@ -68,7 +68,7 @@ public class DoorCheck {
 	 * @return true if the Player is inside the door boundaries, otherwise false
 	 */
 	public boolean doorSouth(final Pair<Integer, Integer> pos) {
-		return (pos.getX() > (GameSettings.TILESIZE * 9) - 11 && pos.getX() < (GameSettings.TILESIZE * 9 ) + 11) && (pos.getY() > GameSettings.HEIGHT - GameSettings.TILESIZE * 3 && pos.getY() < GameSettings.LIMITDOWN);
+		return (pos.getX() > (GameSettings.TILESIZE * 9) - 11 && pos.getX() < (GameSettings.TILESIZE * 9 ) + 11) && (pos.getY() > GameSettings.HEIGHT - GameSettings.TILESIZE * 3);
 	}
 	
 	/**
