9855f404131b1e28578214f51bee2f9844b288d1
Federico Pirazzoli <federico.pirazzoli@studio.unibo.it>
2020-06-22 16:09:04 +0200

    Fixed some pathing issues with the resources and libraries

--- src/coordination/CharacterImage.java
+++ src/coordination/CharacterImage.java
@@ -3,28 +3,40 @@ package coordination;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
+import worldModel.utilities.GameSettings;
+
 public enum CharacterImage {
 
-	FRONT_PLAYER("./res/chars/mainChar6_front.png"), BACK_PLAYER("./res/chars/mainChar6_back.png"),
-	LEFT_PLAYER("./res/chars/mainChar6_left.png"), RIGHT_PLAYER("./res/chars/mainChar6_right.png"),
+	FRONT_PLAYER("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "mainChar6_front.png"), 
+	BACK_PLAYER("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "mainChar6_back.png"),
+	LEFT_PLAYER("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "mainChar6_left.png"), 
+	RIGHT_PLAYER("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "mainChar6_right.png"),
 
-	FRONT_BOWMAN("./res/chars/enemies/Enemy1_front.png"), BACK_BOWMAN("./res/chars/enemies/Enemy1_back.png"),
-	LEFT_BOWMAN("./res/chars/enemies/Enemy1_left.png"), RIGHT_BOWMAN("./res/chars/enemies/Enemy1_right.png"),
+	FRONT_BOWMAN("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy1_front.png"), 
+	BACK_BOWMAN("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy1_back.png"),
+	LEFT_BOWMAN("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy1_left.png"), 
+	RIGHT_BOWMAN("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy1_right.png"),
 
-	FRONT_MAGE("./res/chars/enemies/Enemy3_front.png"), BACK_MAGE("./res/chars/enemies/Enemy3_back.png"),
-	LEFT_MAGE("./res/chars/enemies/Enemy3_left.png"), RIGHT_MAGE("./res/chars/enemies/Enemy3_right.png"),
+	FRONT_MAGE("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy3_front.png"), 
+	BACK_MAGE("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy3_back.png"),
+	LEFT_MAGE("." + GameSettings.SEP + "res" + GameSettings.SEP + "char" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy3_left.png"), 
+	RIGHT_MAGE("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy3_right.png"),
 
-	FRONT_NINJA("./res/chars/enemies/Enemy2_front.png"), BACK_NINJA("./res/chars/enemies/Enemy2_back.png"),
-	LEFT_NINJA("./res/chars/enemies/Enemy2_left.png"), RIGHT_NINJA("./res/chars/enemies/Enemy2_right.png"),
+	FRONT_NINJA("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy2_front.png"), 
+	BACK_NINJA("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy2_back.png"),
+	LEFT_NINJA("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy2_left.png"), 
+	RIGHT_NINJA("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Enemy2_right.png"),
 
-	PLANT("./res/chars/enemies/plan.png"),
+	PLANT("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "plan.png"),
 
-	FRONT_BOSS("./res/chars/enemies/Boss_front.png"), BACK_BOSS("./res/chars/enemies/Boss_back.png"),
-	LEFT_BOSS("./res/chars/enemies/Boss_left.png"), RIGHT_BOSS("./res/chars/enemies/Boss_right.png"),
+	FRONT_BOSS("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Boss_front.png"), 
+	BACK_BOSS("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Boss_back.png"),
+	LEFT_BOSS("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Boss_left.png"), 
+	RIGHT_BOSS("." + GameSettings.SEP + "res" + GameSettings.SEP + "chars" + GameSettings.SEP + "enemies" + GameSettings.SEP + "Boss_right.png"),
 
-	PLAYER_BULLET("./res/proj/MainProj.png"),
+	PLAYER_BULLET("." + GameSettings.SEP + "res" + GameSettings.SEP + "proj" + GameSettings.SEP + "MainProj.png"),
 	
-	ENEMY_BULLET("./res/proj/EnemyProj.png");
+	ENEMY_BULLET("." + GameSettings.SEP + "res" + GameSettings.SEP + "proj" + GameSettings.SEP + "EnemyProj.png");
 	
 	String image;
 
--- src/coordination/SoundBoard.java
+++ src/coordination/SoundBoard.java
@@ -12,11 +12,14 @@ import worldModel.utilities.GameSettings;
  */
 public enum SoundBoard {
 	
-	mainCharacterHurt("." + GameSettings.SEP + "res" + GameSettings.SEP + "audio" + GameSettings.SEP + "mainChar" + GameSettings.SEP + "hurtSound.wav"), mainCharacterShoot("./res/audio/bow/bow_fired.wav"),
-	enemyDamageHurt("./res/audio/enemy/takeDamage.wav"),
-	mainCharacterFootsteps("./res/audio/footsteps/footsteps.wav"), 
-	keyPickUp("./res/audio/pickups/keyPickup.wav"), coinPickUp("./res/audio/pickups/coinPickup.wav"), modPickUp("./res/audio/pickups/modifierPickup.wav"),
-	doorOpen("./res/audio/doors/door_open.wav");
+	mainCharacterHurt("." + GameSettings.SEP + "res" + GameSettings.SEP + "audio" + GameSettings.SEP + "mainChar" + GameSettings.SEP + "hurtSound.wav"), 
+	mainCharacterShoot("." + GameSettings.SEP + "res" + GameSettings.SEP + "audio" + GameSettings.SEP + "bow" + GameSettings.SEP + "bow_fired.wav"),
+	enemyDamageHurt("." + GameSettings.SEP + "res"  + GameSettings.SEP + "audio"  + GameSettings.SEP + "enemy"  + GameSettings.SEP + "takeDamage.wav"),
+	mainCharacterFootsteps("." + GameSettings.SEP + "res" + GameSettings.SEP + "audio" + GameSettings.SEP + "footsteps" + GameSettings.SEP + "footsteps.wav"), 
+	keyPickUp("." + GameSettings.SEP + "res"  + GameSettings.SEP + "audio" + GameSettings.SEP + "pickups"  + GameSettings.SEP + "keyPickup.wav"), 
+	coinPickUp("." + GameSettings.SEP + "res" + GameSettings.SEP + "audio" + GameSettings.SEP + "pickups" + GameSettings.SEP + "coinPickup.wav"), 
+	modPickUp("." + GameSettings.SEP + "res" + GameSettings.SEP + "audio" + GameSettings.SEP + "pickups" + GameSettings.SEP + "modifierPickup.wav"),
+	doorOpen("." + GameSettings.SEP + "res" + GameSettings.SEP + "audio" + GameSettings.SEP + "doors"  + GameSettings.SEP + "door_open.wav");
 	
 	String sound;
 	
